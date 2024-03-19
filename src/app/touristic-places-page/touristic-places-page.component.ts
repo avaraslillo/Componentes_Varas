078ca6ce343d60437eb5b52defa4792f38c11008
avaraslillo <a.varaslillo@hotmail.com>
2024-03-18 23:21:09 -0300

    Ajustes en carrusel de imágenes

--- src/app/touristic-places-page/touristic-places-page.component.ts
+++ src/app/touristic-places-page/touristic-places-page.component.ts
@@ -11,4 +11,14 @@ export class TouristicPlacesPageComponent {
   imagenNantes: string = 'assets/images/Ile_de_Nantes.jfif';
   imagenParis: string = 'assets/images/paris_37bc088a.jpg';
   imagenBruselas: string = 'assets/images/Turismo-en-Bruselas-1030x687.jpg';
+  imagenAmsterdam: string = 'assets/images/amsterdam.jpg';
+  imagenLondres: string = 'assets/images/52372-londres-tower-bridge.jpeg';
+  imagenEdimburgo: string = 'assets/images/edimburgo.jpg';
+  imagenDublin: string = 'assets/images/dublin.jpeg';
+  imagenRoma: string = 'assets/images/roma.jpeg';
+  imagenFlorencia: string = 'assets/images/que-ver-florencia-lugares-imprescindibles.jpg';
+  imagenMilan: string = 'assets/images/que-ver-en-milan.jpeg';
+  imagenVenecia: string = 'assets/images/Historia-de-Venecia-scaled.jpg';
+  imagenCalgary: string = 'assets/images/Calgary.jpeg';
+  imagenToronto: string = 'assets/images/HEADER-TORONTO_2.jpg';
 }
